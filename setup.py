450f59caa4c1bc5d458e4d6e3dd28b227aff9037
brightlego <brightlego@github.com>
2023-12-25 01:32:02 +0000

    Removed unneccessary repitition of version specification from setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import Extension, setup
 import numpy as np
 
 ext = Extension("fastroc", ["fastrocmodule.c"], include_dirs=[np.get_include()])
-setup(name="fastroc", version="1.0.0", ext_modules=[ext])
+setup(name="fastroc", ext_modules=[ext])
